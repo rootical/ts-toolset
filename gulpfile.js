d433c039418fc06aedd86826886c424ba9dc2692
Vadzim Yakushau <rootical.web@gmail.com>
2016-04-14 17:58:40 +0200

    Use gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,39 +1,50 @@
-const gulp = require('gulp');
-const del = require('del');
-const tsc = require('gulp-typescript');
-const sourcemaps = require('gulp-sourcemaps');
-const tsProject = tsc.createProject('tsconfig.json');
-const tslint = require('gulp-tslint');
+'use strict';
+
+let gulp = require('gulp');
+let del = require('del');
+let tsc = require('gulp-typescript');
+let sourcemaps = require('gulp-sourcemaps');
+let tsProject = tsc.createProject('tsconfig.json');
+let tslint = require('gulp-tslint');
 
 /**
- * Remove build directory.
+ * Remove dist directory.
  */
 gulp.task('clean', (cb) => {
-    return del(['build'], cb);
+    return del(['dist'], cb);
 });
 
 /**
- * Compile TypeScript sources and create sourcemaps in build directory.
+ * Lint all custom TypeScript files.
  */
-gulp.task('compile', () => {
-    var tsResult = gulp.src('src/**/*.ts')
+gulp.task('tslint', () => {
+    return gulp.src('src/**/*.ts')
+        .pipe(tslint())
+        .pipe(tslint.report('prose'));
+});
+
+/**
+ * Compile TypeScript sources and create sourcemaps in dist directory.
+ */
+gulp.task('compile', ['tslint'], () => {
+    let tsResult = gulp.src('src/**/*.ts')
         .pipe(sourcemaps.init())
         .pipe(tsc(tsProject));
-return tsResult.js
-    .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('build'));
+    return tsResult.js
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('dist'));
 });
 
 /**
- * Copy all resources that are not TypeScript files into build directory.
+ * Copy all resources that are not TypeScript files into dist directory.
  */
 gulp.task('resources', () => {
     return gulp.src(['src/**/*', '!**/*.ts'])
-        .pipe(gulp.dest('build'))
+        .pipe(gulp.dest('dist'));
 });
 
 /**
- * Copy all required libraries into build directory.
+ * Copy all required libraries into dist directory.
  */
 gulp.task('libs', () => {
     return gulp.src([
@@ -46,12 +57,24 @@ gulp.task('libs', () => {
             'angular2/bundles/angular2.dev.js',
             'angular2/bundles/router.dev.js'
         ], {cwd: 'node_modules/**'}) /* Glob required here. */
-        .pipe(gulp.dest('build/lib'));
+        .pipe(gulp.dest('dist/lib'));
+});
+
+/**
+ * Watch for changes in TypeScript, HTML and CSS files.
+ */
+gulp.task('watch', function () {
+    gulp.watch(['src/**/*.ts'], ['compile']).on('change', function (e) {
+        console.log('TypeScript file ' + e.path + ' has been changed. Compiling.');
+    });
+    gulp.watch(['src/**/*.html', 'src/**/*.css'], ['resources']).on('change', function (e) {
+        console.log('Resource file ' + e.path + ' has been changed. Updating.');
+    });
 });
 
 /**
  * Build the project.
  */
 gulp.task('build', ['compile', 'resources', 'libs'], () => {
-    console.log('Building the project ...')
+    console.log('Building the project ...');
 });
